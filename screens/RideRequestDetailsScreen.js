66e39eccca2f1cac2eca94c1f9817a74ec07de3e
zoecabrales <zoe@old.st>
2024-03-20 18:42:36 +0800

    created folders for screens, components and navigation

---
+++ screens/RideRequestDetailsScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const RideRequestDetailsScreen = () => {
+  return (
+    <View>
+      <Text>RideRequestDetailsScreen</Text>
+    </View>
+  );
+};
+
+export default RideRequestDetailsScreen;
