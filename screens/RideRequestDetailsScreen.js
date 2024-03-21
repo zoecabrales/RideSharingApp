1965e44ecdc921bb85eaf5e44ca503e217046a47
zoecabrales <zoe@old.st>
2024-03-21 09:45:01 +0800

    fixed code for styling

--- screens/RideRequestDetailsScreen.js
+++ screens/RideRequestDetailsScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import CustomButton from "../components/customButton"; // Import the CustomButton component
+import { View, Text } from "react-native";
+import CustomButton from "../components/customButton";
 import dummyRideRequests from "../components/DummyRideData";
+import { styles } from "./rideRequestDetailsStyles"; // Import the styles
 
 const RideRequestDetailsScreen = ({ route }) => {
   const { requestId } = route.params;
@@ -52,45 +53,5 @@ const RideRequestDetailsScreen = ({ route }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 20,
-  },
-  screenTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 5,
-    marginTop: 10,
-  },
-  note: {
-    fontSize: 12,
-    color: "#888",
-    textAlign: "center",
-    marginBottom: 20,
-  },
-  infoContainer: {
-    borderRadius: 20,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    padding: 10,
-    marginBottom: 20,
-  },
-  textContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 5,
-  },
-  label: {
-    fontWeight: "bold",
-    marginRight: 10,
-  },
-  info: {
-    fontSize: 16,
-  },
-});
 
 export default RideRequestDetailsScreen;
--- screens/rideRequestDetailsStyles.js
+++ screens/rideRequestDetailsStyles.js
@@ -1,57 +1,41 @@
-import React from "react";
-import { View, Text } from "react-native";
-import CustomButton from "../components/customButton";
-import dummyRideRequests from "../components/DummyRideData";
-import { styles } from "../styles/rideRequestDetailsStyles"; // Import the styles
+import { StyleSheet } from "react-native";
 
-const RideRequestDetailsScreen = ({ route }) => {
-  const { requestId } = route.params;
-  const rideRequest = dummyRideRequests.find(
-    (request) => request.id === requestId
-  );
-
-  const handleAcceptBooking = () => {
-    console.log("Booking accepted");
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.screenTitle}>Accept a booking right away!</Text>
-      <Text style={styles.note}>
-        You can't accept a booking unless it's pending. But hey, keep looking,
-        there could be a booking any minute now!
-      </Text>
-      <View style={styles.infoContainer}>
-        <View style={styles.textContainer}>
-          <Text style={styles.label}>Ride ID:</Text>
-          <Text style={styles.info}>{rideRequest.id}</Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.label}>Status:</Text>
-          <Text style={styles.info}>{rideRequest.status}</Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.label}>PickUp:</Text>
-          <Text style={styles.info}>
-            {rideRequest.pickupLocation.latitude},{" "}
-            {rideRequest.pickupLocation.longitude}
-          </Text>
-        </View>
-        <View style={styles.textContainer}>
-          <Text style={styles.label}>Destination:</Text>
-          <Text style={styles.info}>
-            {rideRequest.destination.latitude},{" "}
-            {rideRequest.destination.longitude}
-          </Text>
-        </View>
-      </View>
-      <CustomButton
-        onPress={handleAcceptBooking}
-        disabled={rideRequest.status !== "pending"}
-        title="Accept Booking"
-      />
-    </View>
-  );
-};
-
-export default RideRequestDetailsScreen;
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  screenTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+    marginTop: 10,
+  },
+  note: {
+    fontSize: 12,
+    color: "#888",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  infoContainer: {
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 20,
+  },
+  textContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 5,
+  },
+  label: {
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  info: {
+    fontSize: 16,
+  },
+});
